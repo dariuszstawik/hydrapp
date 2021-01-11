5e5631a041179eb3b57339fc5b0ba761a687be99
dariuszstawik <dariusz.stawik@gmail.com>
2021-01-11 20:25:46 +0100

    hydrapp v1

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,50 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
-/* place your code below */
 
-console.log('HELLO 🚀')
+const buttonAdd = document.querySelector('.app__button-add--js');
 
-const buttonadd = document.querySelector('.app__button-add--js');
+const buttonRemove = document.querySelector('.app__button-remove--js');
 
-const counterPresent = document.querySelector('.app__cup__counter--js')
+const cupCounter = document.querySelector('.app__cup__counter--js'); 
 
 let counter = 0;
 
-    const myClick = () => {
-        console.log('kliknąłeś');
-        counter++
-        console.log(counter);
-        counterPresent.innerHTML = counter;
-        
-    }
+const key = new Date().toISOString().slice(0,10);
 
-    buttonadd.addEventListener('click', myClick);
+let localStorageValue = localStorage.getItem(key);
+
+console.log(localStorageValue);
+
+if (localStorageValue) {
+    counter = localStorageValue;
+}
+
+else {
+    counter = 0;
+}
+
+console.log(counter);
+
+localStorage.setItem(key, counter);
+
+let addCup = () => {
+    counter++
+    cupCounter.innerHTML = counter;    
+    localStorage.setItem(key, counter);
+}
+
+let removeCup = () => {
+    
+    if (counter > 0) {
+        counter--
+        cupCounter.innerHTML = counter;
+        localStorage.setItem(key, counter);
+    }    
+}
+
+buttonAdd.addEventListener('click', addCup);
+buttonRemove.addEventListener('click', removeCup);
